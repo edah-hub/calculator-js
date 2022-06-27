2ebbd5c806a458c2c7538184d7c0c8d050a9cddf
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-06-27 14:22:31 +0300

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-// *** Write your code here***
+function display(value) {
+    document.getElementById("result").value += value;
+}
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
